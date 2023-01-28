abfd1555c247606b8192529d6440d1d3f157673c
loverstea <115397195+loverstea@users.noreply.github.com>
2023-01-28 11:04:21 +0200

    Update My_app_na_kivy.py

--- My_app_na_kivy.py
+++ My_app_na_kivy.py
@@ -10,16 +10,28 @@ class TaskListScreen(Screen):
 
 class NewTaskScreen(Screen):
     ...
-
+class Example(MDApp):
+    data = {
+        'Python': 'language-python',
+        'PHP': 'language-php',
+        'C++': 'language-cpp',
+    }
 class ToDoApp(MDApp):
+    data = {
+            'Python': 'language-python',
+            'PHP': 'language-php',
+            'C++': 'language-cpp',
+        }
+
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Purple"
         Builder.load_file('graf.kv')
 
+
         sm = ScreenManager()
         sm.add_widget(TaskListScreen(name="main_screen"))
+        sm.add_widget(NewTaskScreen(name="second_screen"))
         return sm
-
 app = ToDoApp()
 app.run()
