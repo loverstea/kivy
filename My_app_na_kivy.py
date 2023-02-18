742ee809f6eef8f1de4582b6602d141baca7da8f
loverstea <115397195+loverstea@users.noreply.github.com>
2023-02-18 13:08:41 +0200

    Update My_app_na_kivy.py

--- My_app_na_kivy.py
+++ My_app_na_kivy.py
@@ -11,7 +11,13 @@ from kivymd.uix.list import ILeftBodyTouch, TwoLineAvatarIconListItem
 from kivymd.uix.selectioncontrol import MDCheckbox
 
 from kivy.storage.jsonstore import JsonStore
+from random import randint
 
+from kivy.utils import platform
+
+if platform == "android":
+    from android.permissions import request_permissions, Permission
+    request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
 class LeftCheckbox(ILeftBodyTouch, MDCheckbox):
     ...
 class TaskItem(TwoLineAvatarIconListItem):
@@ -20,6 +26,13 @@ class TaskItem(TwoLineAvatarIconListItem):
         self.key = key
     def delete_task(self, the_task_item):
         self.parent.remove_widget(the_task_item)
+        app.db.delete(self.key)
+    def chek_task(self, check, list_item):
+        if check.active == True:
+            app.db.put(self.key,task = self.text, date=self.secondary_text, done=True)
+        else:
+            app.db.put(self.key,task = self.text, date=self.secondary_text, done=False)
+
 class TaskListScreen(Screen):
     ...
 
@@ -59,17 +72,24 @@ class ToDoApp(MDApp):
         self.db = JsonStore("DB.json")
         #self.db.put(1, task="ДОмашка", date="11/2/2023", done = False)
         self.tasks = []
-        keys = self.db.keys()
-        for i in keys:
+        self.keys = self.db.keys()
+        for i in self.keys:
             if self.db.exists(i):
                 self.tasks.append(self.db.get(i))
                 new_task = TaskItem(key = i, text=self.tasks[-1]['task'], secondary_text=self.tasks[-1]['date'])
+                if self.tasks[-1]['done'] == True:
+                    new_task.ids.check.active = True
                 self.screen.ids.task_list.add_widget(new_task)
     
     def add_task(self, text, date):
         if text != "":
-            new_task = TaskItem(key = len(self.tasks), text=text , secondary_text=date)
+            new_key = randint(0,10000)
+            while self.db.exists(new_key):
+                new_key = randint(0,10000)
+
+            new_task = TaskItem(key = new_key, text=text , secondary_text=date)
             self.screen.ids.task_list.add_widget(new_task)
+            self.db.put(new_key,task = text, date=date, done=False)
             self.screen.manager.transition.direction = "down"
             self.screen.manager.transition.duration = 0.7
             self.screen.manager.current = "main_screen"
